e43b5851108d5d03ac2196dda68bccefd4bd336e
Min <80498105+minahl@users.noreply.github.com>
2021-04-12 22:32:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ client = Client(account_sid, auth_token)
 message = client.messages.create(
                               body='Yay! Push event triggered in master branch',
                               from_='whatsapp:+14155238886',
-                              to='whatsapp:'+os.environ['to_whatsapp_no']
+                              to='whatsapp:+6596669675']
                           )
 
 print("Message ID:",message.sid)
